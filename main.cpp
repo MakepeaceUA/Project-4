efd716518dadf25ea235a8c2c20bca48eb32508d
Arsen Sevastyanov <130586920+Makepease@users.noreply.github.com>
2023-05-14 16:23:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+
+//ДЗ№1
+//int main() {
+//setlocale(0, "RUS");
+
+//double Film;
+//int Speed;
+    
+//cout << "Введите размер фильма в Гигабайтах:\n";
+//cin >> Film;
+    
+//cout << "Введите скорость интернет-соединения в Мегабитах в секунду:\n";
+//cin >> Speed;
+    
+//double MB = Film * 8000;
+//double Time = MB / Speed;
+    
+//int Hr = Time / 3600;
+//int Min = (Time - (Hr * 3600)) / 60;
+//int Sec = Time - (Hr * 3600) - (Min * 60);
+    
+//cout << "Фильм будет скачан за:\n" << Hr << " часов," << Min << " минут," << Sec << " секунд.";
+//}
+
+
+
+
+
+
+//ДЗ№2
+//int main() {
+//setlocale(0, "RUS");
+
+//int Flash_Drive;
+    
+//cout << "Введите объём флешки в Гигабайтах:\n";
+//cin >> Flash_Drive;
+    
+//int MB = Flash_Drive * 1000;
+//int Films = MB / 760;
+
+//cout << "Количество фильмов,которое поместится в флешке:\n" << Films;
+//}
+
+
+
+
+
+//ДЗ№3
+//int main() {
+//setlocale(0, "RUS");
+
+//float number;
+//cout << "Введите дробное число:\n";
+//cin >> number;
+
+//int round = (number * 100)+ 0.5;
+//float res = round / 100.0;
+
+//cout << "Округленное число:" << res;
+//}
+
+
+
+
+
+//ДЗ№4
+//int main() {
+//setlocale(0, "RUS");
+
+//int Pass;
+//int Fail;
+   
+//cout << "Введите количество студентовa,которые сдали ДЗ:\n";
+//cin >> Pass;
+    
+//cout << "Введите количество должников:\n";
+//cin >> Fail;
+
+//int Total_Student = Pass + Fail;
+//cout << "Общее количество студентов:" << Total_Student << "\n";
+    
+//float Res = (Fail * 100.0) / Total_Student;
+//cout << "Должники составляют " << Res << "% от общего числа студентов.";
+//}
+
+
+
+
+
+
+
+//ДЗ№5
+//int main() {
+//setlocale(0, "RUS");
+
+//int Sec;
+    
+//cout << "Введите количество прошедших секунд:\n";
+//cin >> Sec;
+
+//int R_Sec_1 = 86400 - Sec;
+//int R_Hr = R_Sec_1 / 3600;
+//int R_Min =  (R_Sec_1 % 3600) / 60;
+//int R_Sec_2 = (R_Sec_1 % 3600) % 60;
+
+//cout << "До полуночи осталось:" << R_Hr << " часов, " << R_Min << " минут, " << R_Sec_2 << " секунд.";
+//}
+
+
+
+
+
+
+
+
+//ДЗ№7
+//int main() {
+  
+//const double PI = 3.14;
+//const double R = 6378.1;
+
+//int Area= 4 * PI * R * R;
+//cout << "Площадь поверхности Земли: " << Area << " квадратных километров\n";
+//}
+
+
